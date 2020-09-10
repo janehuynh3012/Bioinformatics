9d9ee03bd6d6eff21dba57dbffaf623f0b78bb57
janehuynh3012 <67124631+janehuynh3012@users.noreply.github.com>
2020-09-10 22:09:23 +0700

    Add files via upload

---
+++ reversecomplement.py
@@ -0,0 +1,19 @@
+# Input:	A DNA	string Pattern.
+# Output: The reverse complement	of	Pattern.
+def ReverseComplement(Pattern):
+	 Pattern	= Complement(Pattern) #	complement each letter in a string	  
+	 Pattern	= Reverse(Pattern) #	reverse all	letters in a string
+	 return Pattern
+def Reverse(Pattern):
+    # your code	here
+    reverse = '' ”	#create an empty string
+    for char	in	Pattern:
+        reverse = char	+ reverse #the	string is reversed
+    return reverse
+def Complement(Pattern):
+	 #	your code here
+	 complement	= “ “
+	 basepair =	{“A”:”T”, “T”:”A”, “C”:”G”, “G”:”C”}
+	 for char in Pattern:
+		  complement += basepair.get(char) #return the value for	basepair
+	 return complement
